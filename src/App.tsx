36d86c527b8aeb3b48c13fa0ccf977183e5a19c2
= <=>
2024-03-02 19:01:41 -0500

    useState hook with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,42 @@
 import "./App.css";
-import CourseGoal from "./components/CourseGoal";
 import Header from "./components/Header";
 import goalsImage from "./assets/goals.jpg";
+import { useState } from "react";
+import CourseGoal from "./components/CourseGoal";
 const App = () => {
+    type CourseGoal = {
+        title: string;
+        description: string;
+        id: number;
+    };
+    const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+    const handleAddGoal = () => {
+        setGoals((previousGoals) => {
+            const newGoal: CourseGoal = {
+                title: "Learn React + TS",
+                description: "Learn it in depth!",
+                id: Math.random(),
+            };
+            return [newGoal, ...previousGoals];
+        });
+    };
+
     return (
         <main>
             <Header image={{ src: goalsImage, alt: "Goals Image here!" }}>
                 <h1>Your Course Goals here!</h1>
             </Header>
-            <CourseGoal title="Learn React + TS Course">
-                <p>Learn it from the ground up children!!</p>
-            </CourseGoal>
+            <button onClick={handleAddGoal}>Add Goal</button>
+            <ul>
+                {goals.map((goal) => (
+                    <li key={goal.id}>
+                        <CourseGoal title={goal.title}>
+                            <p>{goal.description}</p>
+                        </CourseGoal>
+                    </li>
+                ))}
+            </ul>
         </main>
     );
 };
